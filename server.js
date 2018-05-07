a81f8b921e948ac4e4af09e67eae27de8c306566
ceoehis <ceo.ehis@outlook.com>
2018-05-07 11:08:01 +0100

    initialize counter table

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+var Counter = require('./models/counter');
 //set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/urlshortenerDB';
@@ -8,6 +9,26 @@ mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+// check db to see if counter collection exists
+Counter.find({_id: 'counter_table'}, function(err, res) {
+  if(err) {
+    console.error(err);
+  }
+  if(res.length) {
+    // table already exists; skip creating
+    console.log('counter table already exist', res);
+    return;
+  }
+  // initialize counter collection with starting value
+  var counter = new Counter({
+    count: 10000
+  })
+  counter.save(function(err) {
+    if(err) throw err;
+    console.log('counter initialized');
+  });
+})
 
 var shortenRouter = require('./routes/shorten');
 
