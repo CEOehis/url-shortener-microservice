d09ced02e5873f49676f9f1e77d1f1b400ac8d48
ceoehis <ceo.ehis@outlook.com>
2018-05-07 09:09:27 +0100

    connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+//set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/urlshortenerDB';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
 var shortenRouter = require('./routes/shorten');
 
 var port = process.env.PORT || 3000;
