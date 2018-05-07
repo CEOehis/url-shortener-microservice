0dbe4434d52e8b278d9b380f911b2f4a1ed6f7bf
ceoehis <ceo.ehis@outlook.com>
2018-05-07 11:08:18 +0100

    add presave step

--- models/url.js
+++ models/url.js
@@ -1,12 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Counter = require('./counter');
 
 var UrlSchema = new Schema({
-  url: {type: String}
+  _id: Number,
+  url: String
 }, {
   timestamps: {
     createdAt: 'created'
   }
+});
+
+UrlSchema.pre('save', function(next) {
+  // use arrow function to have access to 'this'(which points to the url instance)
+  Counter.findByIdAndUpdate({_id: 'counter_table'}, { $inc: { count: 1 }}, {}, (err, counter) =>{
+    console.log('inside arrow', this);
+    if(err) {
+      return next(err);
+    }
+    this._id = counter.count;
+    return next();
+  });
 });
 
 module.exports = mongoose.model('Url', UrlSchema);
